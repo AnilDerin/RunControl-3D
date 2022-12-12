4f60f11c00491c52960391f773ddf97fbd5b57db
Anıl Derinbay <aderinbay54@gmail.com>
2022-12-12 15:23:21 +0300

    Update MainMenu_Manager.cs

--- Assets/MainMenu_Manager.cs
+++ Assets/MainMenu_Manager.cs
@@ -10,7 +10,7 @@ public class MainMenu_Manager : MonoBehaviour
 
     public GameObject QuitPanel;
 
-    //MathOps _MathOps = new MathOps();
+    MathOps _MathOps = new MathOps();
 
     // Start is called before the first frame update
     void Start()
